73a83935ac8f43b18349a8b2320aa312f5878643
Corentin FERRY <c.ferry78@laposte.net>
2026-06-01 12:02:29 +0200

    feat: Repository with JPA for users

---
+++ src/main/java/com/campus/Users_Square_Games/web/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.campus.Users_Square_Games.web.repository;
+
+import com.campus.Users_Square_Games.web.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User>findByUsername(String username);
+    Optional<User>findByEmail(String email);
+    boolean existsById(Long id);
+}
