b235e3c5107dc2876ecf460c63818ee8f415920e
Corentin FERRY <c.ferry78@laposte.net>
2026-06-01 12:03:49 +0200

    feat: UserController for endpoints

---
+++ src/main/java/com/campus/Users_Square_Games/web/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.campus.Users_Square_Games.web.controller;
+
+import com.campus.Users_Square_Games.web.model.User;
+import com.campus.Users_Square_Games.web.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // POST /users - Créer un utilisateur
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return userService.createUser(user);
+    }
+
+    // GET /users/{id} - Récupérer un utilisateur par son identifiant
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable Long id){
+        return userService.getUserById(id);
+    }
+
+    // DELETE /users/{id} - Supprimer un utilisateur
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id){
+        userService.deletedUser(id);
+    }
+
+    // GET /users/{id}/valid - Vérifier qu'un id existe
+    @GetMapping("/{id}/valid")
+    public boolean userExists (@PathVariable Long id){
+        return userService.userExists(id);
+    }
+
+}
