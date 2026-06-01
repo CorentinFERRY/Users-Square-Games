9db2558e33e6320d053a76a4c72bfd658ed3578f
Corentin FERRY <c.ferry78@laposte.net>
2026-06-01 10:17:35 +0200

    Initial commit

---
+++ src/main/java/com/campus/Users_Square_Games/UsersSquareGamesApplication.java
@@ -0,0 +1,13 @@
+package com.campus.Users_Square_Games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersSquareGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersSquareGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campus/Users_Square_Games/UsersSquareGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.Users_Square_Games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersSquareGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
