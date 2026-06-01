f38c1ff0438f37b3ad1d97a88cb3cd7937410ce6
Corentin FERRY <c.ferry78@laposte.net>
2026-06-01 12:01:36 +0200

    feat: add Entity for users in the project

---
+++ src/main/java/com/campus/Users_Square_Games/web/model/User.java
@@ -0,0 +1,28 @@
+package com.campus.Users_Square_Games.web.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    // Constructeur avec champs obligatoires (sans id)
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+}
